509d563aaf279598244be66221ec6d25f8fb7e1b
AadiXC0DE <aadityaz2077@gmail.com>
2023-07-30 20:44:57 +0530

    added alt tags

--- app/components/Brands.jsx
+++ app/components/Brands.jsx
@@ -17,17 +17,17 @@ export default function Brands() {
     <div className="bg-transparent flex justify-center w-full mt-20">
       <div className="w-[1566.82px] relative flex gap-12 overflow-hidden">
         <div className="w-80 h-full absolute left-0 top-0 bg-gradient-to-r from-black"></div>
-        <Image src={image1} />
-        <Image src={image2} />
-        <Image src={image3} />
-        <Image src={image4} />
-        <Image src={image5} />
-        <Image src={image6} />
-        <Image src={image7} />
-        <Image src={image8} />
-        <Image src={image9} />
-        <Image src={image10} />
-        <Image src={image11} />
+        <Image src={image1} alt="ex-brand-1" />
+        <Image src={image2} alt="ex-brand-2" />
+        <Image src={image3} alt="ex-brand-3" />
+        <Image src={image4} alt="ex-brand-4" />
+        <Image src={image5} alt="ex-brand-5" />
+        <Image src={image6} alt="ex-brand-6" />
+        <Image src={image7} alt="ex-brand-7" />
+        <Image src={image8} alt="ex-brand-8" />
+        <Image src={image9} alt="ex-brand-9" />
+        <Image src={image10} alt="ex-brand-10" />
+        <Image src={image11} alt="ex-brand-11" />
         <div className="w-80 h-full absolute right-0 top-0 bg-gradient-to-l from-black"></div>
       </div>
     </div>
